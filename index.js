3b787da39de71e4fa0e1710b1ea32fc3153d93c9
ipinnuoluwa <ipintoye@gmail.com>
2023-09-16 19:09:55 +0100

    JavaScript

---
+++ index.js
@@ -0,0 +1,75 @@
+// alert("Hello Word");
+// document.write("Hello Septa People");
+// console.log("Heyy guys, what you up to")
+
+// login.code
+console.log("Hiya")
+
+var name = "Ipinnu";
+
+console.log(name)
+
+
+// Comment
+
+// statement
+
+// variable
+// var
+// let
+// const
+
+var school = "unilag";
+
+console.log(school);
+
+let car;
+
+car = "toyota";
+car = "honda"
+
+const shape = "triangle"
+
+// params or argument or placeholder
+
+function addition(params){
+    console.log("Heyy guys")
+}
+
+addition()  //invoke or call argument
+
+function greet(name) {
+  console.log(name)
+}
+greet("Gabriel")
+
+function names(countryName, abbreviation) {
+  console.log(countryName + ' ' + abbreviation);
+}
+
+names('Nigeria', 'Nig');
+names('Togo', 'Tog');
+names('America', 'Ame');
+names('Dubai', 'Dub');
+
+
+function add(a, b) {
+	console.log(a + " + " + b + " = ", a + b)
+
+}
+
+add(1,2);
+
+function multiply(a, b) {
+	console.log(a + " * " + b + " = ", a*b)
+
+}
+
+multiply(3,4);
+
+function subtract(a, b) {
+	console.log(a + " - " + b + " = ", a-b)
+
+}
+
+subtract(6,1);
